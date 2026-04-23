f30439b5b7c93ce30ca4095c81a8d87abc896ecb
alphsa-1 <alphsa01@gmail.com>
2026-04-24 00:06:48 +0800

    @update added login/signup and renamed  variables

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify, render_template
 import sqlite3
 
+# Flask framework start
 app = Flask(__name__)
 
+# Connects to database
 def get_db_connection(db_name):
     conn = sqlite3.connect(
         db_name,
@@ -18,7 +20,8 @@ def init_db():
     cursor.executescript("""         
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL UNIQUE
+        name TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL
     );
 
     CREATE TABLE IF NOT EXISTS subjects (
@@ -66,6 +69,8 @@ def init_db():
 
     conn.commit()
 
+## Database Manipulation (CRUD)
+# Manager for the services, connects to the database and executes lines
 class DatabaseManager:
     def __init__(self, db_name="src/grades.db"):
         self.conn = get_db_connection(db_name)
@@ -80,17 +85,22 @@ class DatabaseManager:
     def close(self):
         self.conn.close()
 
+# Handles user-related database manipulation
 class UserService:
     def __init__(self, db: DatabaseManager):
         self.db = db
 
-    def create(self, name):
-        self.db.execute(
-            "INSERT INTO users (name) VALUES (?)",
-            (name,),
+    # Creates user entry
+    def create(self, name, password):
+        cursor = self.db.execute(
+            "INSERT INTO users (name, password) VALUES (?, ?)",
+            (name, password),
             commit=True
         )
 
+        return cursor.lastrowid
+
+    # Gets user 
     def get(self, user_id):
         return self.db.execute(
             "SELECT * FROM users WHERE id=?",
@@ -104,6 +114,7 @@ class UserService:
             commit=True
         )
     
+    # Gets user id by name
     def get_user_id_by_name(self, name):
         row = self.db.execute(
             "SELECT id FROM users WHERE name=?",
@@ -112,11 +123,13 @@ class UserService:
 
         return row["id"] if row else None
 
+# Handles per-subject
 class SubjectService:
     def __init__(self, db: DatabaseManager, grade_service):
         self.db = db
         self.grade_service = grade_service
 
+    # Creates a subject entry
     def create(self, user_id, name, unit):
         cursor = self.db.execute(
             "INSERT INTO subjects (user_id, name, unit) VALUES (?, ?, ?)",
@@ -125,6 +138,7 @@ class SubjectService:
         )
         return cursor.lastrowid
 
+    # Returns a subject from query
     def get_subject(self, user_id, name):
         subject = self.db.execute(
             "SELECT * FROM subjects WHERE user_id = (?) AND name = (?)",
@@ -134,6 +148,7 @@ class SubjectService:
 
         return subject
 
+    # Updates subject name or unit
     def update(self, subject_id, name=None, unit=None):
         if name:
             self.db.execute(
@@ -148,6 +163,7 @@ class SubjectService:
                 commit=True
             )
 
+    # Deletes a subject if needed
     def delete(self, subject_id):
         self.db.execute(
             "DELETE FROM subjects WHERE id=?",
@@ -155,6 +171,7 @@ class SubjectService:
             commit=True
         )
 	
+    # Gets the entire data of a subject
     def get_full_subjects(self, user_id):
         subjects = self.db.execute(
             "SELECT id, name, unit FROM subjects WHERE user_id=?",
@@ -175,11 +192,14 @@ class SubjectService:
 
         return result
 
+# Handles quarters - items
 class GradeService:
     def __init__(self, db: DatabaseManager):
         self.db = db
 
-    # QUARTER
+    ## QUARTER
+
+    # Creates a quarter entry (need to call individually four times)
     def create_quarter(self, subject_id, quarter):
         cursor = self.db.execute(
             "INSERT INTO quarters (subject_id, quarter) VALUES (?, ?)",
@@ -188,6 +208,7 @@ class GradeService:
         )
         return cursor.lastrowid
 
+    # Returns 4 quarters of a subject from a query
     def get_quarters(self, subject_id):
         quarters = self.db.execute(
             "SELECT id, quarter, grade, passed FROM quarters WHERE subject_id=?",
@@ -208,6 +229,7 @@ class GradeService:
 
         return result
 
+    # Returns a specific quarter using its number and subject id
     def get_quarter(self, quarter_number, subject_id):
         quarter = self.db.execute(
             "SELECT * FROM quarters WHERE quarter = ? AND subject_id = ?",
@@ -216,8 +238,10 @@ class GradeService:
 
         return quarter
 
-    # SA / FA
-    def create_assessment_type(self, quarter_id, type_, percentage):
+    ## SA / FA (Category)
+
+    # Creates category entry (call two times individually)
+    def create_category(self, quarter_id, type_, percentage):
         cursor = self.db.execute(
             """INSERT INTO categories (quarter_id, type, percentage)
                VALUES (?, ?, ?)""",
@@ -226,7 +250,8 @@ class GradeService:
         )
         return cursor.lastrowid
 
-    def get_assessment_types(self, quarter_id):
+    # Gets the two categories of a quarter
+    def get_categories(self, quarter_id):
         rows = self.db.execute(
             "SELECT id, type, percentage FROM categories WHERE quarter_id=?",
             (quarter_id,)
@@ -245,7 +270,8 @@ class GradeService:
 
         return result
 
-    def get_assessment_type(self, quarter_id, type):
+    # Get specifc category using type and quarter id
+    def get_category(self, quarter_id, type):
         assessment_type = self.db.execute(
             "SELECT * FROM categories WHERE quarter_id = ? AND type = ?",
             (quarter_id, type,)
@@ -254,20 +280,23 @@ class GradeService:
         print(assessment_type)
         return assessment_type
 
-    # CATEGORY
-    def create_category(self, assessment_type_id, label, percentage):
+    ## SUB-CATEGORY
+
+    # Creates a sub-category entry
+    def create_sub_category(self, category_id, label, percentage):
         cursor = self.db.execute(
             """INSERT INTO sub_categories (assessment_type_id, label, percentage)
                VALUES (?, ?, ?)""",
-            (assessment_type_id, label, percentage),
+            (category_id, label, percentage),
             commit=True
         )
         return cursor.lastrowid
 
-    def get_categories(self, assessment_type_id):
+    # Returns all sub-categories of a category
+    def get_sub_categories(self, category_id):
         rows = self.db.execute(
             "SELECT id, label, percentage FROM sub_categories WHERE assessment_type_id=?",
-            (assessment_type_id,)
+            (category_id,)
         ).fetchall()
 
         result = []
@@ -283,24 +312,28 @@ class GradeService:
 
         return result
 
-    def get_category(self, assessment_type_id, label):
+    # Returns a category based off its label and category id
+    def get_sub_category(self, category_id, label):
         category = self.db.execute(
             "SELECT * FROM sub_categories WHERE assessment_type_id = ? AND label = ?",
-            (assessment_type_id, label,)
+            (category_id, label,)
         ).fetchone()
 
         return category
 
-    # ITEM
-    def create_item(self, category_id, label, score, max_score):
+    ## ITEM
+
+    # Creates an item entry
+    def create_item(self, sub_category_id, label, score, max_score):
         self.db.execute(
             """INSERT INTO items 
                (category_id, label, score_obtained, maximum_score)
                VALUES (?, ?, ?, ?)""",
-            (category_id, label, score, max_score),
+            (sub_category_id, label, score, max_score),
             commit=True
         )
 
+    # Updates the items value
     def update_item(self, item_id, score, max_score):
         self.db.execute(
             """UPDATE items 
@@ -310,6 +343,7 @@ class GradeService:
             commit=True
         )
 
+    # Deletes an item entry
     def delete_item(self, item_id):
         self.db.execute(
             "DELETE FROM items WHERE id=?",
@@ -317,11 +351,12 @@ class GradeService:
             commit=True
         )
 
-    def get_items(self, category_id):
+    # Gets all items under a sub_category
+    def get_items(self, sub_category_id):
         rows = self.db.execute(
             """SELECT label, score_obtained, maximum_score 
             FROM items WHERE category_id=?""",
-            (category_id,)
+            (sub_category_id,)
         ).fetchall()
 
         return [
@@ -333,10 +368,11 @@ class GradeService:
             for r in rows
         ]
 
-    def get_item(self, category_id, label):
+    # Gets a specific item based off its label and sub-category id
+    def get_item(self, sub_category_id, label):
         item = self.db.execute(
             "SELECT * FROM items WHERE category_id = ? AND label = ?",
-            (category_id, label,)
+            (sub_category_id, label,)
         )
 
         return item
@@ -346,6 +382,37 @@ db = DatabaseManager()
 user_service = UserService(db)
 grade_service = GradeService(db)
 subject_service = SubjectService(db, grade_service)
+
+def signup(name, password):
+    global _user_id
+
+    # Creates new user
+    _user_id = user_service.create(name, password)
+
+def login(name, password):
+    global _user_id
+
+    # Searches for matching credentials
+    conn = get_db_connection("src/grades.db")
+    cursor = conn.cursor()
+    user = cursor.execute(
+        "SELECT * FROM users WHERE name = ? AND password = ?",
+        (name, password,)
+    ).fetchone()
+
+    # Logins successfully
+    if user is not None:
+        _user_id = user["id"]
+    else:
+        return 401 # Unsuccessful Login Attempt
+
+def create_empty_subject_data(user_id, name, unit):
+    subject_id = subject_service.create(user_id, name, unit)
+    for i in range(1, 5):
+        quarter_id = grade_service.create_quarter(subject_id, i)
+        grade_service.create_category(quarter_id, "SA", 0)
+        grade_service.create_category(quarter_id, "FA", 0)
+
 
 # Inject test data
 @app.route("/seed")
@@ -358,15 +425,15 @@ def seed_data():
     quarter = grade_service.get_quarter(1, subj_id)
     quarter_id = quarter["id"]
 
-    grade_service.create_assessment_type(quarter_id, "SA", 0.7)
-    grade_service.create_assessment_type(quarter_id, "FA", 0.3)
+    grade_service.create_category(quarter_id, "SA", 0.7)
+    grade_service.create_category(quarter_id, "FA", 0.3)
 
-    at = grade_service.get_assessment_type(quarter_id, "SA")
+    at = grade_service.get_category(quarter_id, "SA")
     at_id = at["id"]
 
-    grade_service.create_category(at_id, "Long Test", 0.5)
+    grade_service.create_sub_category(at_id, "Long Test", 0.5)
 
-    cat = grade_service.get_category(at_id, "Long Test")
+    cat = grade_service.get_sub_category(at_id, "Long Test")
     cat_id = cat["id"]
 
     grade_service.create_item(cat_id, "Long Test 1", 37, 40)
